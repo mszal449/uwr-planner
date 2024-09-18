5e66e221cb9f1a1e6da69aaee2626a84af9ca682
mszal449 <m.szalasz@gmail.com>
2024-09-18 21:27:18 +0200

    Refactor scrollbars and page layout

--- planner-app/app/page.tsx
+++ planner-app/app/page.tsx
@@ -17,14 +17,14 @@ interface ObjectId {
 
 export default function Home() {
   return (
-    <div className="h-screen flex flex-col px-5">
+    <div className="h-screen flex flex-col px-2 overflow-hidden">
       <div className="flex-grow grid grid-cols-12 gap-4 text-xl mb-0">
         {/* courses browser and semester planner */}
         <CourseBrowser styles={"max-h-[95vh] col-start-1 col-end-3 py-5"} />
         <SemesterPlanner styles={"p-5 col-start-3 col-end-13"} />
       </div>
       {/* summary */}
-      <Summary styles=""/>
+      <Summary styles="h-full m-0"/>
     </div>
   );
 }
--- planner-app/components/CourseBrowser/CourseBrowser.tsx
+++ planner-app/components/CourseBrowser/CourseBrowser.tsx
@@ -3,18 +3,23 @@ import { CourseBrowserProps } from '@/types'
 import { CourseCard } from '@/components'
 import { sample_courses } from '@/const'
 import { Course } from '@/types'
-import { ScrollArea, Scrollbar } from '@radix-ui/react-scroll-area'
+import { ScrollArea, Scrollbar} from '@radix-ui/react-scroll-area'
+
+ 
+const tags = Array.from({ length: 50 }).map(
+  (_, i, a) => `v1.2.0-beta.${a.length - i}`
+)
+ 
 
 const CourseBrowser = ({styles}: CourseBrowserProps) => {
   return (
-    // todo: make height somehow better
     <div className={`${styles} flex flex-col gap-2`}>
       <div className='flex'>
         <input type="text" placeholder='search' className='font-light p-2 rounded-tl-md rounded-bl-md outline-none w-full bg-[#282828] focus:bg-[#363636] transition ease-in'/>
         <button className='bg-[#282828] rounded-tr-md rounded-br-md px-3 border-none hover:bg-[#363636] transition ease-in'>O</button>
       </div>
 
-      <div className='flex flex-col gap-2 overflow-scroll'>
+      <ScrollArea className='flex flex-col gap-2 h-full overflow-auto '>
         {sample_courses.map((course: Course) => (
           <CourseCard
             key={course._id.$oid}
@@ -25,8 +30,10 @@ const CourseBrowser = ({styles}: CourseBrowserProps) => {
             tags={course.tags}
             effects={course.effects? course.effects : null}
         />))}
+        <Scrollbar orientation='vertical' />
+      </ScrollArea>
+
     </div>
-  </div>
   )
 }
 
