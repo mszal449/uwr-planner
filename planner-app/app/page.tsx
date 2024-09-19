6f04a092404efe87df800e0156b9fa815daf4679
Hortensjaa <lilka305@gmail.com>
2024-09-19 11:24:03 +0200

    PUT request service

--- planner-app/app/page.tsx
+++ planner-app/app/page.tsx
@@ -1,37 +1,10 @@
 'use client';
-import { getCourses } from './services';
-import { CourseI } from '@/types';
-
-import { useEffect, useState } from 'react';
-import Head from 'next/head';
 
 
 export default function Home() {
-  const [courses, setCourses] = useState<CourseI[]>([]);
-
-  useEffect(() => {
-    const fetchCourses = async () => {
-      const data  = await getCourses({name: "A ", tags: ["AZ (algorytmika i złożoność obliczeniowa)"]});
-      setCourses(data.result);
-    };
-    fetchCourses();
-  }, []);
-
 
   return (
     <div>
-      <Head>
-        <title>Courses List</title>
-      </Head>
-      <h1>Courses</h1>
-      <br />
-      <ul>
-        {courses.map((course) => (
-          <li key={course._id}>
-            {course.name}
-          </li>
-        ))}
-      </ul>
     </div>
   );
 };
---
+++ planner-app/services/userService.ts
@@ -0,0 +1,41 @@
+import { DataI, PlanI, UserI } from '@/types'
+import { getAll, addRecord, getById, getWithFilters} from './servicesUtils';
+
+
+const urlString = "users"
+
+export async function getAllUsers(): Promise<DataI<UserI>> {
+    return getAll(urlString)
+}
+  
+export async function getUsers(filters: Record<string, any>): Promise<DataI<UserI>> {
+    return getWithFilters(urlString, filters)
+}
+
+export async function getUserById(id: string): Promise<UserI | null> {
+    return getById(urlString, id) 
+}
+
+export async function addUser(user: UserI): Promise<UserI> {
+    return addRecord(urlString, user)
+}
+
+export async function updatePlan(user: UserI, plan: PlanI, id: string) {
+    const updatedPlans = user.plans?.map(p => p._id === id ? { ...p, ...plan } : p) || [];
+    const updatedUser = {
+        ...user,
+        plans: updatedPlans
+    };
+    const response = await fetch(`http://localhost:3000/api/${urlString}`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(updatedUser)
+    });
+    if (!response.ok) {
+        throw new Error(`Error: status ${response.status}`);
+    }
+    const data = await response.json();
+    return data;
+}
