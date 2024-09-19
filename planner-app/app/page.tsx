54fe07511cf6336d8de964aa108355c06785fd3b
mszal449 <m.szalasz@gmail.com>
2024-09-19 10:39:25 +0200

    Fixed main page layout

--- planner-app/app/page.tsx
+++ planner-app/app/page.tsx
@@ -17,14 +17,14 @@ interface ObjectId {
 
 export default function Home() {
   return (
-    <div className="h-screen flex flex-col px-2 overflow-hidden">
-      <div className="flex-grow grid grid-cols-12 gap-4 text-xl mb-0">
-        {/* courses browser and semester planner */}
-        <CourseBrowser styles={"max-h-[95vh] col-start-1 col-end-3 py-5"} />
-        <SemesterPlanner styles={"p-5 col-start-3 col-end-13"} />
+    <div className="flex flex-col h-screen "> {/* Use h-screen to take the full screen */}
+      <div className="flex-grow grid grid-cols-12 gap-4 p-5 pb-0 min-h-0"> {/* Set min-h-0 to prevent overflow */}
+        {/* CourseBrowser and SemesterPlanner */}
+        <CourseBrowser styles="col-start-1 col-end-3 overflow-auto" />
+        <SemesterPlanner styles="col-start-3 col-end-13" />
       </div>
-      {/* summary */}
-      <Summary styles="h-full m-0"/>
+      {/* Summary with fixed or limited height */}
+      <Summary styles="mt-3 h-10"/> {/* You can adjust the height as needed */}
     </div>
   );
 }
