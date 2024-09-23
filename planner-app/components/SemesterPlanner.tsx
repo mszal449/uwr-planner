e7c6d17eee171937e950f6581b665ea6870efa25
mszal449 <m.szalasz@gmail.com>
2024-09-23 13:55:19 +0200

    Styled ects counter

--- planner-app/components/SemesterPlanner.tsx
+++ planner-app/components/SemesterPlanner.tsx
@@ -1,23 +1,23 @@
 'use client'
-import React from 'react'
+import React, { useEffect } from 'react'
 import { SemesterPlannerProps } from '@/types';
 import CourseCard from './CourseBrowser/CourseCard';
 
 const SemesterPlanner = ( { styles, plan, onSelectSemester, onSelectCourse , deleteCourse, selectedCourseId} : SemesterPlannerProps) => {
-  const calculateEcts = (semId: number) => {
-    let ects = 0
-    if (plan?.semesters[semId]) {
-        plan.semesters[semId].forEach(course => {
-          ects += course.ects
-        
-      });
-    }
-    if (semId !== 0) {
-      ects += calculateEcts(semId - 1)
-    }
+  const [ects, setEcts] = React.useState([0, 0, 0, 0, 0, 0])
+  useEffect(() => {
+    let ects = [0, 0, 0, 0, 0, 0]
+    plan?.semesters.forEach((semester, index) => {
+      semester.forEach(course => {
+        ects[index] += course.ects
+      })
+      if (index > 0) {
+        ects[index] += ects[index - 1]
+      }
+    })
+    setEcts(ects)
+  }, [plan])
 
-    return ects
-  }
 
   return (
     <div className={`${styles} flex justify-between gap-2 overflow-x-scroll `}>
@@ -27,7 +27,7 @@ const SemesterPlanner = ( { styles, plan, onSelectSemester, onSelectCourse , del
             <div className='text-3xl font-light truncate'>
               {index + 1} Semestr 
             </div>
-            <div className='text-gray-400 truncate'>{calculateEcts(index)}/{30 * (index + 1)} ECTS</div> 
+            <div className={`text-gray-400 truncate ${(ects[index] >= 30 * (index + 1)) ? "text-green-500" : 'text-red-500'}`}>{ects[index]}/{30 * (index + 1)} ECTS</div>
           </div>
           <div className='flex flex-col gap-1'>
             {semester.map((course) => (
