06f82902aa382d63c7660eca94c0eb8b787b5d6b
mszal449 <m.szalasz@gmail.com>
2024-10-04 09:49:21 +0200

    Second code restructure commit

--- planner-app/models/index.ts
+++
@@ -1,7 +0,0 @@
-// Export models
-import Course from "./course"
-import Plan from "./plan"
-
-export {
-    Course, Plan
-}
--- planner-app/services/index.ts
+++
@@ -1,7 +0,0 @@
-import { getAllCourses, getCourseById  } from "./courseService";
-import { getAllPlans, getPlanById, getUserPlans, addPlan, updatePlan, deletePlan } from "./planService";
-
-export {
-    getAllCourses, getCourseById,
-    getAllPlans, getPlanById, getUserPlans, addPlan, updatePlan, deletePlan
-}
